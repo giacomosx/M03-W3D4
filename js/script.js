2808251330f97016ed6a671ded08ddd35c3b2647
= <giacomo.bartoli@me.com>
2024-02-13 01:15:01 +0100

    script fix

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
-window.onload = function () {
-    customCarousel()
-}
+const btnPrev = document.querySelectorAll('.prevBtn');
+const btnNext = document.querySelectorAll('.nextBtn');
 
-function customCarousel() {
-    const btnPrev = document.querySelectorAll('.prevBtn');
-    const btnNext = document.querySelectorAll('.nextBtn');
-
-    btnPrev.forEach(btn => {
-        btn.addEventListener('click', function () {
-            let idSlider = this.getAttribute('data-g-slider');
-            const slidesWrapper = document.getElementById(idSlider);
-            slidesWrapper.style.translate = 0;
-        })
+btnPrev.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let idSlider = this.getAttribute('data-g-slider');
+        const slidesWrapper = document.getElementById(idSlider);
+        slidesWrapper.style.translate = 0;
+        
     })
+})
 
 
-    btnNext.forEach(btn => {
-        btn.addEventListener('click', function () {
-            let idSlider = this.getAttribute('data-g-slider');
-            const slidesWrapper = document.getElementById(idSlider);
-            slidesWrapper.style.translate = '-' + slidesWrapper.offsetWidth + 'px';
-        })
+btnNext.forEach(btn => {
+    btn.addEventListener('click', function () {
+        let idSlider = this.getAttribute('data-g-slider');
+        const slidesWrapper = document.getElementById(idSlider);
+        slidesWrapper.style.translate = '-' + slidesWrapper.offsetWidth + 'px';
     })
-}
+})
